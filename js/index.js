35776b65803f9e25081188bdc04fe16435177274
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 19:41:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,14 +4,9 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-
-
 const input = document.querySelector('input');
  
 input.addEventListener('keydown', function(e) {
-  if (e.key === "g") {
-    return e.preventDefault()
-  } else {
-    console.log(e.key)
-  }
+  console.log(e.key);
 });
+
